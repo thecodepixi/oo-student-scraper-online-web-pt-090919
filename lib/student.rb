057450d999bc2e02c10951170a104e0550c394aa
Emily Harber <emily.a.harber@gmail.com>
2019-10-20 03:46:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 class Student
 
   attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
@@ -10,9 +12,7 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    students_array.each do |student|
-      Student.new(student)
-    end 
+    binding.pry 
   end
 
   def add_student_attributes(attributes_hash)
